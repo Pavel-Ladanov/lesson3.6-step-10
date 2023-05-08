e8ee546d188b9228fff588360e6a45267b4a7466
Pavel-Ladanov <p-ladanov@yandex.ru>
2023-05-08 13:46:58 +0300

    add files for task

---
+++ test_items.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+import time
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_button_add_to_basket_exists(browser):
+    browser.get(link)
+    #time.sleep(30)
+    button = browser.find_element(By.CSS_SELECTOR, "#add_to_basket_form > button")
+    assert button != None
